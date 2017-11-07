f54a6137952fc3d452e12d84fd02ba1587ac9a87
mtrentman <mike.trentman@alveohealth.com>
2017-11-07 14:24:33 -0500

    Code cleanup

--- src/com/mtlogic/AuthenticationManager.java
+++ src/com/mtlogic/AuthenticationManager.java
@@ -18,50 +18,49 @@ import org.slf4j.LoggerFactory;
 @Path("/api")
 public class AuthenticationManager {
 	final Logger logger = LoggerFactory.getLogger(AuthenticationManager.class);
-	
+
 	@Path("/token")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response requestToken(@Context HttpServletRequest requestContext, String inputMessage) throws JSONException 
-	{	
+	public Response requestToken(@Context HttpServletRequest requestContext, String inputMessage) throws JSONException {
 		logger.info(">>>ENTERED requestToken()");
 		Response response = null;
 		String token = null;
 
 		int responseCode = HttpStatus.SC_ACCEPTED;
-		
+
 		try {
 			AuthenticationService tokenManager = new AuthenticationService();
 			token = tokenManager.requestToken(inputMessage);
 		} catch (Exception e) {
 			logger.error("Message could not be processed: " + e.getMessage());
 			e.printStackTrace();
-			response = Response.status(HttpStatus.SC_UNPROCESSABLE_ENTITY).entity("Message could not be processed: " + e.getMessage()).build();
+			response = Response.status(HttpStatus.SC_UNPROCESSABLE_ENTITY)
+					.entity("Message could not be processed: " + e.getMessage()).build();
 		}
-		
+
 		if (token == null) {
 			responseCode = HttpStatus.SC_UNAUTHORIZED;
 		}
 		response = Response.status(responseCode).entity(token).build();
-		
+
 		logger.info("<<<EXITED requestToken()");
 		return response;
 	}
-	
+
 	@Path("/token/validate")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response validateToken(String inputMessage) throws JSONException 
-	{	
+	public Response validateToken(String inputMessage) throws JSONException {
 		logger.info(">>>ENTERED validateToken()");
 		Response response = null;
 		String token = null;
 		String responseString = null;
 
 		int responseCode = HttpStatus.SC_OK;
-		
+
 		try {
 			JSONObject obj = new JSONObject(inputMessage);
 			token = obj.getString("token");
@@ -70,100 +69,101 @@ public class AuthenticationManager {
 		} catch (Exception e) {
 			logger.error("Message could not be processed: " + e.getMessage());
 			e.printStackTrace();
-			response = Response.status(HttpStatus.SC_UNPROCESSABLE_ENTITY).entity("Message could not be processed: " + e.getMessage()).build();
+			response = Response.status(HttpStatus.SC_UNPROCESSABLE_ENTITY)
+					.entity("Message could not be processed: " + e.getMessage()).build();
 		}
-		
+
 		if (token == null || responseString.contains("false")) {
 			responseCode = HttpStatus.SC_UNAUTHORIZED;
 		}
 		response = Response.status(responseCode).entity(responseString).build();
-		
+
 		logger.info("<<<EXITED validateToken()");
 		return response;
 	}
-	
+
 	@Path("/credentials")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response addCredentials(String inputMessage) throws JSONException 
-	{	
+	public Response addCredentials(String inputMessage) throws JSONException {
 		logger.info(">>>ENTERED addCredentials()");
 		Response response = null;
 		String statusMessage = null;
 
 		int responseCode = HttpStatus.SC_ACCEPTED;
-		
+
 		try {
 			AuthenticationService tokenManager = new AuthenticationService();
 			statusMessage = tokenManager.addCredentials(inputMessage);
 		} catch (Exception e) {
 			logger.error("Message could not be processed: " + e.getMessage());
 			e.printStackTrace();
-			response = Response.status(HttpStatus.SC_UNPROCESSABLE_ENTITY).entity("Message could not be processed: " + e.getMessage()).build();
+			response = Response.status(HttpStatus.SC_UNPROCESSABLE_ENTITY)
+					.entity("Message could not be processed: " + e.getMessage()).build();
 		}
-		
+
 		if (response == null) {
 			response = Response.status(responseCode).entity(statusMessage).build();
 		}
 		logger.info("<<<EXITED addCredentials()");
 		return response;
 	}
-	
+
 	@Path("/password")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response resetPassword(String inputMessage) throws JSONException 
-	{	
+	public Response resetPassword(String inputMessage) throws JSONException {
 		logger.info(">>>ENTERED resetPassword()");
 		Response response = null;
 		String statusMessage = null;
 
 		int responseCode = HttpStatus.SC_ACCEPTED;
-		
+
 		try {
 			AuthenticationService tokenManager = new AuthenticationService();
 			statusMessage = tokenManager.resetPassword(inputMessage);
 		} catch (Exception e) {
 			logger.error("Message could not be processed: " + e.getMessage());
 			e.printStackTrace();
-			response = Response.status(HttpStatus.SC_UNPROCESSABLE_ENTITY).entity("Message could not be processed: " + e.getMessage()).build();
+			response = Response.status(HttpStatus.SC_UNPROCESSABLE_ENTITY)
+					.entity("Message could not be processed: " + e.getMessage()).build();
 		}
-		
+
 		if (response == null) {
 			response = Response.status(responseCode).entity(statusMessage).build();
 		}
 		logger.info("<<<EXITED resetPassword()");
 		return response;
 	}
-	
+
 	@Path("/password/overwrite")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response overwritePassword(String inputMessage) throws JSONException 
-	{	
+	public Response overwritePassword(String inputMessage) throws JSONException {
 		logger.info(">>>ENTERED overwritePassword()");
 		Response response = null;
 		String statusMessage = null;
 
 		int responseCode = HttpStatus.SC_ACCEPTED;
-		
+
 		try {
 			AuthenticationService tokenManager = new AuthenticationService();
 			statusMessage = tokenManager.overwritePassword(inputMessage);
 		} catch (Exception e) {
 			logger.error("Message could not be processed: " + e.getMessage());
 			e.printStackTrace();
-			response = Response.status(HttpStatus.SC_UNPROCESSABLE_ENTITY).entity("Message could not be processed: " + e.getMessage()).build();
+			response = Response.status(HttpStatus.SC_UNPROCESSABLE_ENTITY)
+					.entity("Message could not be processed: " + e.getMessage()).build();
 		}
-		
+
 		if (response == null) {
 			response = Response.status(responseCode).entity(statusMessage).build();
 		}
 		logger.info("<<<EXITED overwritePassword()");
 		return response;
 	}
-	
+
 }
--- src/com/mtlogic/AuthenticationService.java
+++ src/com/mtlogic/AuthenticationService.java
@@ -40,9 +40,10 @@ public class AuthenticationService {
 	public static final String NEWPASSWORD_REQUIRED = "--NEWPASSWORD IS REQUIRED!--";
 	public static final String EMAIL_REQUIRED = "--EMAIL IS REQUIRED!--";
 	public static final String CLIENT_NUMBER_REQUIRED = "--CLIENT NUMBER IS REQUIRED!--";
-	
-	public AuthenticationService() {}
-	
+
+	public AuthenticationService() {
+	}
+
 	public String requestToken(String jsonMessage) {
 		logger.info(">>>ENTERED requestToken()");
 		final JSONObject obj = new JSONObject(jsonMessage);
@@ -51,7 +52,7 @@ public class AuthenticationService {
 		String token = null;
 		StringBuilder sb = new StringBuilder();
 		String clientId = verifyCredentials(user, password);
-		
+
 		if (INVALID_CREDENTIALS.equals(clientId) || RESET_PASSWORD.equals(clientId)) {
 			sb.append("{ \"error\":\"");
 			sb.append(clientId);
@@ -59,21 +60,21 @@ public class AuthenticationService {
 		} else {
 			token = generateToken(user, clientId);
 			if (token != null) {
-this.verifyToken(token);				
+				this.verifyToken(token);
 				sb.append("{ \"token\":\"");
 				sb.append(token);
 				sb.append("\" }");
 			}
-		} 
-		
+		}
+
 		logger.info("<<<EXITED requestToken(" + sb.toString() + ")");
 		return sb.toString();
 	}
-	
+
 	public String addCredentials(String jsonMessage) {
 		logger.info(">>>ENTERED addCredentials(");
 		StringBuilder sb = new StringBuilder();
-		//String status = ADDED_USER;
+		// String status = ADDED_USER;
 		Vector<String> errorList = new Vector<String>();
 		final JSONObject obj = new JSONObject(jsonMessage);
 		String user = null;
@@ -95,29 +96,29 @@ this.verifyToken(token);
 		} catch (Exception e) {
 			errorList.add(CLIENT_NUMBER_REQUIRED);
 		}
-		
+
 		if (errorList.isEmpty()) {
-			Integer clientId = lookupClientId(clientNumber); 
-			if (clientId != null) {		
+			Integer clientId = lookupClientId(clientNumber);
+			if (clientId != null) {
 				Context envContext = null;
 				Connection con = null;
 				PreparedStatement preparedStatement = null;
 				String insertUserSQL = "insert into public.user (user_name, email_address, password, client_id) values(?, ?, ?, ?)";
-				
+
 				try {
 					envContext = new InitialContext();
-					Context initContext  = (Context)envContext.lookup("java:/comp/env");
+					Context initContext = (Context) envContext.lookup("java:/comp/env");
 					DataSource ds = null;
-					ds = (DataSource)initContext.lookup("jdbc/admin");
-					
-					con = ds.getConnection();					
+					ds = (DataSource) initContext.lookup("jdbc/admin");
+
+					con = ds.getConnection();
 					preparedStatement = con.prepareStatement(insertUserSQL);
 					preparedStatement.setString(1, user);
 					preparedStatement.setString(2, email);
 					preparedStatement.setString(3, SecureHash.generateStrongPasswordHash(password));
 					preparedStatement.setInt(4, clientId);
-					
-					preparedStatement.executeUpdate();		
+
+					preparedStatement.executeUpdate();
 				} catch (SQLException e) {
 					e.printStackTrace();
 					if (e.getMessage().contains("user_user_name_key")) {
@@ -133,26 +134,34 @@ this.verifyToken(token);
 					e.printStackTrace();
 					logger.error("ERROR!!! : " + e.getMessage());
 				} finally {
-				    try{preparedStatement.close();}catch(Exception e){};
-				    try{con.close();}catch(Exception e){};
+					try {
+						preparedStatement.close();
+					} catch (Exception e) {
+					}
+					;
+					try {
+						con.close();
+					} catch (Exception e) {
+					}
+					;
 				}
 			} else {
 				errorList.addElement(CLIENT_DOES_NOT_EXIST);
 			}
 		}
-		
+
 		sb.append("{ \"status\":\"");
 		if (errorList.isEmpty()) {
 			sb.append(ADDED_USER);
-		} else { 
+		} else {
 			sb.append(errorList.toString());
 		}
 		sb.append("\" }");
-		
+
 		logger.info("<<<EXITED addCredentials(" + sb.toString() + ")");
 		return sb.toString();
 	}
-	
+
 	public String verifyCredentials(String user, String password) {
 		logger.info(">>>ENTERED verifyCredentials()");
 		Context envContext = null;
@@ -160,22 +169,22 @@ this.verifyToken(token);
 		PreparedStatement preparedStatement = null;
 		String clientId = null;
 		Boolean resetPassword = false;
-		 
+
 		if ((user != null && !user.isEmpty()) && (password != null && !password.isEmpty())) {
 			String selectQuery = "select reset_password from public.user where user_name = ?";
-			
+
 			try {
 				envContext = new InitialContext();
-				Context initContext  = (Context)envContext.lookup("java:/comp/env");
+				Context initContext = (Context) envContext.lookup("java:/comp/env");
 				DataSource ds = null;
-			    ds = (DataSource)initContext.lookup("jdbc/admin");
+				ds = (DataSource) initContext.lookup("jdbc/admin");
 				con = ds.getConnection();
-				
+
 				preparedStatement = con.prepareStatement(selectQuery);
 				preparedStatement.setString(1, user);
-				
+
 				ResultSet rs = preparedStatement.executeQuery();
-				
+
 				if (rs.next()) {
 					resetPassword = rs.getBoolean("reset_password");
 				}
@@ -184,7 +193,7 @@ this.verifyToken(token);
 				} else {
 					clientId = RESET_PASSWORD;
 				}
-				
+
 			} catch (SQLException e) {
 				e.printStackTrace();
 				logger.error("ERROR!!! : " + e.getMessage());
@@ -195,15 +204,23 @@ this.verifyToken(token);
 				e.printStackTrace();
 				logger.error("ERROR!!! : " + e.getMessage());
 			} finally {
-			    try{preparedStatement.close();}catch(Exception e){};
-			    try{con.close();}catch(Exception e){};
+				try {
+					preparedStatement.close();
+				} catch (Exception e) {
+				}
+				;
+				try {
+					con.close();
+				} catch (Exception e) {
+				}
+				;
 			}
 		}
 
 		logger.info("<<<EXITED verifyCredentials(" + clientId + ")");
-        return (clientId);
-    }
-	
+		return (clientId);
+	}
+
 	private String verifyPassword(String user, String password) {
 		logger.info(">>>ENTERED verifyPassword()");
 		boolean isValid = false;
@@ -212,22 +229,22 @@ this.verifyToken(token);
 		PreparedStatement preparedStatement = null;
 		String storedPassword = null;
 		String clientNumber = null;
-		 
+
 		if ((user != null && !user.isEmpty()) && (password != null && !password.isEmpty())) {
 			String selectSQL = "select password, client_id from public.user where user_name = ?";
-			
+
 			try {
 				envContext = new InitialContext();
-				Context initContext  = (Context)envContext.lookup("java:/comp/env");
+				Context initContext = (Context) envContext.lookup("java:/comp/env");
 				DataSource ds = null;
-				ds = (DataSource)initContext.lookup("jdbc/admin");
+				ds = (DataSource) initContext.lookup("jdbc/admin");
 				con = ds.getConnection();
-				
+
 				preparedStatement = con.prepareStatement(selectSQL);
 				preparedStatement.setString(1, user);
-				
+
 				ResultSet rs = preparedStatement.executeQuery();
-				
+
 				if (rs.next()) {
 					storedPassword = rs.getString("password");
 					clientNumber = rs.getString("client_id");
@@ -242,24 +259,32 @@ this.verifyToken(token);
 				e.printStackTrace();
 				logger.error("ERROR!!! : " + e.getMessage());
 			} finally {
-			    try{preparedStatement.close();}catch(Exception e){};
-			    try{con.close();}catch(Exception e){};
+				try {
+					preparedStatement.close();
+				} catch (Exception e) {
+				}
+				;
+				try {
+					con.close();
+				} catch (Exception e) {
+				}
+				;
 			}
-			
+
 			try {
 				Boolean valid = SecureHash.validatePassword(password, storedPassword);
-		        if ((valid)) {
-		        	isValid = true;
-		        }
+				if ((valid)) {
+					isValid = true;
+				}
 			} catch (Exception e) {
 				isValid = false;
 			}
 		}
 
 		logger.info("<<<EXITED verifyPassword(" + isValid + ")");
-        return (isValid?clientNumber:INVALID_CREDENTIALS);
-    }
-	
+		return (isValid ? clientNumber : INVALID_CREDENTIALS);
+	}
+
 	public String resetPassword(String jsonMessage) {
 		logger.info(">>>ENTERED resetPassword()");
 		List<String> statusList = new ArrayList<String>();
@@ -270,7 +295,7 @@ this.verifyToken(token);
 		} catch (Exception e) {
 			statusList.add(USERNAME_REQUIRED);
 		}
-		String password = null; 
+		String password = null;
 		try {
 			password = obj.getString("password");
 		} catch (Exception e) {
@@ -278,7 +303,7 @@ this.verifyToken(token);
 		}
 		String newPassword = null;
 		try {
-			newPassword= obj.getString("newpassword");
+			newPassword = obj.getString("newpassword");
 		} catch (Exception e) {
 			statusList.add(NEWPASSWORD_REQUIRED);
 		}
@@ -288,40 +313,41 @@ this.verifyToken(token);
 		} catch (Exception e) {
 			statusList.add(EMAIL_REQUIRED);
 		}
-		
-		//StringBuilder sb = new StringBuilder();
+
+		// StringBuilder sb = new StringBuilder();
 		if (statusList.isEmpty()) {
 			Boolean initialized = verifyUserIsInitialized(user);
-			
+
 			String status = "SUCCESS";
 			if (initialized) {
 				status = verifyPassword(user, password);
 			}
-			
+
 			if (!INVALID_CREDENTIALS.equals(status)) {
-				//Removed the check below for now so that users can use their previous password
-				//status = checkPasswordStrength(password, newPassword);
+				// Removed the check below for now so that users can use their
+				// previous password
+				// status = checkPasswordStrength(password, newPassword);
 				status = null;
-				
+
 				if (status == null) {
 					Context envContext = null;
 					Connection con = null;
 					PreparedStatement preparedStatement = null;
 					String updateSQL = "update public.user set password = ?, email_address = ?, reset_password = false where user_name = ?";
-					
+
 					try {
 						envContext = new InitialContext();
-						Context initContext  = (Context)envContext.lookup("java:/comp/env");
+						Context initContext = (Context) envContext.lookup("java:/comp/env");
 						DataSource ds = null;
-						ds = (DataSource)initContext.lookup("jdbc/admin");
-						
+						ds = (DataSource) initContext.lookup("jdbc/admin");
+
 						con = ds.getConnection();
 						preparedStatement = con.prepareStatement(updateSQL);
-						
+
 						preparedStatement.setString(1, SecureHash.generateStrongPasswordHash(newPassword));
 						preparedStatement.setString(2, email);
 						preparedStatement.setString(3, user);
-						
+
 						preparedStatement.executeUpdate();
 						status = UPDATED_PASSWORD;
 						statusList.add(UPDATED_PASSWORD);
@@ -335,8 +361,16 @@ this.verifyToken(token);
 						e.printStackTrace();
 						logger.error("ERROR!!! : " + e.getMessage());
 					} finally {
-					    try{preparedStatement.close();}catch(Exception e){};
-					    try{con.close();}catch(Exception e){};
+						try {
+							preparedStatement.close();
+						} catch (Exception e) {
+						}
+						;
+						try {
+							con.close();
+						} catch (Exception e) {
+						}
+						;
 					}
 				} else {
 					statusList.add(status);
@@ -345,39 +379,39 @@ this.verifyToken(token);
 				statusList.add(INVALID_CREDENTIALS);
 			}
 		}
-		
-//		sb.append("{ \"status\":\"");
-//		sb.append(status);
-//		sb.append("\" }");
+
+		// sb.append("{ \"status\":\"");
+		// sb.append(status);
+		// sb.append("\" }");
 		JSONObject jsonObject = new JSONObject();
 		jsonObject.put("status", statusList);
-		 
+
 		logger.info("<<<EXITED resetPassword(" + jsonObject.toString() + ")");
-		//return sb.toString();
+		// return sb.toString();
 		return jsonObject.toString();
 	}
-	
+
 	private Boolean verifyUserIsInitialized(String user) {
 		logger.info(">>>ENTERED verifyUserIsInitialized(" + user + ")");
 		Context envContext = null;
 		Connection con = null;
 		PreparedStatement preparedStatement = null;
 		Boolean initialized = Boolean.FALSE;
-		 
+
 		String selectSQL = "select initialized from public.user where user_name = ?";
-		
+
 		try {
 			envContext = new InitialContext();
-			Context initContext  = (Context)envContext.lookup("java:/comp/env");
+			Context initContext = (Context) envContext.lookup("java:/comp/env");
 			DataSource ds = null;
-			ds = (DataSource)initContext.lookup("jdbc/admin");
+			ds = (DataSource) initContext.lookup("jdbc/admin");
 			con = ds.getConnection();
-			
+
 			preparedStatement = con.prepareStatement(selectSQL);
 			preparedStatement.setString(1, user);
-			
+
 			ResultSet rs = preparedStatement.executeQuery();
-			
+
 			if (rs.next()) {
 				initialized = rs.getBoolean("initialized");
 			}
@@ -391,39 +425,47 @@ this.verifyToken(token);
 			e.printStackTrace();
 			logger.error("ERROR!!! : " + e.getMessage());
 		} finally {
-		    try{preparedStatement.close();}catch(Exception e){};
-		    try{con.close();}catch(Exception e){};
+			try {
+				preparedStatement.close();
+			} catch (Exception e) {
+			}
+			;
+			try {
+				con.close();
+			} catch (Exception e) {
+			}
+			;
 		}
 
 		logger.info("<<<EXITED verifyUserIsInitialized(" + initialized + ")");
 		return initialized;
 	}
-	
+
 	public String overwritePassword(String jsonMessage) {
 		logger.info(">>>ENTERED overwritePassword()");
 		final JSONObject obj = new JSONObject(jsonMessage);
 		String user = obj.getString("username");
 		String newPassword = obj.getString("newpassword");
 		StringBuilder sb = new StringBuilder();
-		
+
 		String status = null;
 		Context envContext = null;
 		Connection con = null;
 		PreparedStatement preparedStatement = null;
 		String updateSQL = "update public.user set password = ?, reset_password = false where user_name = ?";
-		
+
 		try {
 			envContext = new InitialContext();
-			Context initContext  = (Context)envContext.lookup("java:/comp/env");
+			Context initContext = (Context) envContext.lookup("java:/comp/env");
 			DataSource ds = null;
-			ds = (DataSource)initContext.lookup("jdbc/admin");
-			
+			ds = (DataSource) initContext.lookup("jdbc/admin");
+
 			con = ds.getConnection();
 			preparedStatement = con.prepareStatement(updateSQL);
-			
+
 			preparedStatement.setString(1, SecureHash.generateStrongPasswordHash(newPassword));
 			preparedStatement.setString(2, user);
-			
+
 			preparedStatement.executeUpdate();
 			status = UPDATED_PASSWORD;
 		} catch (SQLException e) {
@@ -436,23 +478,31 @@ this.verifyToken(token);
 			e.printStackTrace();
 			logger.error("ERROR!!! : " + e.getMessage());
 		} finally {
-		    try{preparedStatement.close();}catch(Exception e){};
-		    try{con.close();}catch(Exception e){};
-		} 
-		
+			try {
+				preparedStatement.close();
+			} catch (Exception e) {
+			}
+			;
+			try {
+				con.close();
+			} catch (Exception e) {
+			}
+			;
+		}
+
 		sb.append("{ \"status\":\"");
 		sb.append(status);
 		sb.append("\" }");
-		 
+
 		logger.info("<<<EXITED overwritePassword(" + status + ")");
 		return sb.toString();
 	}
-	
+
 	private String checkPasswordStrength(String oldPassword, String newPassword) {
 		logger.info(">>>ENTERED checkPasswordStrength()");
 		String status = null;
 		// Need to determine password rules and add below
-		//	for now just a few simple checks...
+		// for now just a few simple checks...
 		if (oldPassword.equals(newPassword)) {
 			status = PASSWORD_SAME;
 		}
@@ -462,23 +512,23 @@ this.verifyToken(token);
 		logger.info("<<<EXITED checkPasswordStrength(" + status + ")");
 		return status;
 	}
-	
+
 	private Integer lookupClientId(String clientNumber) {
 		logger.info(">>>ENTERED lookupClientId(" + clientNumber + ")");
 		Context envContext = null;
 		Connection con = null;
 		PreparedStatement preparedStatement = null;
 		Integer clientId = null;
-		 
+
 		String selectSQL = "select client_id from public.client where client_number = ?";
-		
+
 		try {
 			envContext = new InitialContext();
-			Context initContext  = (Context)envContext.lookup("java:/comp/env");
+			Context initContext = (Context) envContext.lookup("java:/comp/env");
 			DataSource ds = null;
-			ds = (DataSource)initContext.lookup("jdbc/admin");
+			ds = (DataSource) initContext.lookup("jdbc/admin");
 			con = ds.getConnection();
-			
+
 			preparedStatement = con.prepareStatement(selectSQL);
 			preparedStatement.setString(1, clientNumber);
 
@@ -497,53 +547,59 @@ this.verifyToken(token);
 			e.printStackTrace();
 			logger.error("ERROR!!! : " + e.getMessage());
 		} finally {
-			try{preparedStatement.close();}catch(Exception e){};
-		    try{con.close();}catch(Exception e){};
+			try {
+				preparedStatement.close();
+			} catch (Exception e) {
+			}
+			;
+			try {
+				con.close();
+			} catch (Exception e) {
+			}
+			;
 		}
 
 		logger.info("<<<EXITED lookupClientId(" + clientId + ")");
 		return (clientId);
 	}
-	
+
 	private String generateToken(String userName, String clientId) {
 		logger.info(">>>ENTERED generateToken(" + userName + "' " + clientId + ")");
 		String id = UUID.randomUUID().toString().replace("-", "");
 		Date now = new Date();
-		//Date exp = new Date(System.currentTimeMillis() + (1000 * 60)); // 60 seconds
-		//Date exp = new Date(System.currentTimeMillis() + (1000 * 60)*60); // 60 minutes
-		//Date exp = new Date(System.currentTimeMillis() + (1000 * 60)*60*24); // 24 hours
-		//Date exp = new Date(System.currentTimeMillis() + (1000 * 60)*60*24*30); // 30 days
-		Date exp = new Date(System.currentTimeMillis() + (1000 * 60)*60*24*265); // 265 days
+		// Date exp = new Date(System.currentTimeMillis() + (1000 * 60)); // 60
+		// seconds
+		// Date exp = new Date(System.currentTimeMillis() + (1000 * 60)*60); //
+		// 60 minutes
+		// Date exp = new Date(System.currentTimeMillis() + (1000 * 60)*60*24);
+		// // 24 hours
+		// Date exp = new Date(System.currentTimeMillis() + (1000 *
+		// 60)*60*24*30); // 30 days
+		Date exp = new Date(System.currentTimeMillis() + (1000 * 60) * 60 * 24 * 265); // 265
+																						// days
 
 		// Temporary for test will store secret in database
-		//    maybe have one secret for each client?+
-		//Key secret = MacProvider.generateKey(SignatureAlgorithm.HS256);
-		//byte[] secretBytes = secret.getEncoded();
-		//String base64SecretBytes = Base64.getEncoder().encodeToString(secretBytes);
+		// maybe have one secret for each client?+
+		// Key secret = MacProvider.generateKey(SignatureAlgorithm.HS256);
+		// byte[] secretBytes = secret.getEncoded();
+		// String base64SecretBytes =
+		// Base64.getEncoder().encodeToString(secretBytes);
 		String base64SecretBytes = "JPLet+DhcjalDLlmhpH4Xi0ivIlzj/ZTZVJvGkMB7e8=";
 
-		String token = Jwts.builder()
-				.setId(id)
-				.setIssuedAt(now)
-				.setNotBefore(now)
-				.setExpiration(exp)
-				.setSubject(userName + ":" + clientId)
-				.signWith(SignatureAlgorithm.HS256, base64SecretBytes)
-				.compact();
+		String token = Jwts.builder().setId(id).setIssuedAt(now).setNotBefore(now).setExpiration(exp)
+				.setSubject(userName + ":" + clientId).signWith(SignatureAlgorithm.HS256, base64SecretBytes).compact();
 
 		logger.info("<<<EXITED generateToken(" + token + ")");
 		return token;
 	}
-	
+
 	public String verifyToken(String token) {
 		logger.info(">>>ENTERED verifyToken(" + token + ")");
 		String base64SecretBytes = "JPLet+DhcjalDLlmhpH4Xi0ivIlzj/ZTZVJvGkMB7e8=";
 		boolean isValid = false;
 		Claims claims = null;
 		try {
-			claims = Jwts.parser()
-					.setSigningKey(base64SecretBytes)
-					.parseClaimsJws(token).getBody();
+			claims = Jwts.parser().setSigningKey(base64SecretBytes).parseClaimsJws(token).getBody();
 			if (claims != null) {
 				Date now = new Date();
 				Date exp = claims.getExpiration();
@@ -556,7 +612,7 @@ this.verifyToken(token);
 			e.printStackTrace();
 			isValid = false;
 		}
-		
+
 		String[] bits = null;
 		if (isValid) {
 			bits = claims.getSubject().split(":");
@@ -571,7 +627,7 @@ this.verifyToken(token);
 				isValid = false;
 			}
 		}
-		
+
 		JSONObject obj = new JSONObject();
 		obj.put("verified", isValid);
 		if (isValid) {
